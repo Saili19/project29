54a53b3f7bdfb4592321882011fcd08d2f5ab21e
Saili19 <72297504+Saili19@users.noreply.github.com>
2020-12-18 15:02:46 +0530

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,29 @@
+class Block {
+    constructor(x, y, width, height) {
+      var options = {
+           restitution : 0.8,
+          density: 1,
+          friction :0.4,
+          isStatic:false
+   
+           
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+       
+      push();
+      translate(pos.x, pos.y);
+       
+      rectMode(CENTER);
+      fill(247,88,147);
+       
+      rect(0, 0, this.width, this.height);
+      pop();
+    }
+  } 
